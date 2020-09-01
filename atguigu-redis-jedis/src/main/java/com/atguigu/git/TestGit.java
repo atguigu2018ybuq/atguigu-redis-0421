0bdae87fb49b5b93f9b369b08d0dcfdd8423c4ec
justin <justin@atguigu.com>
2020-09-01 17:16:07 +0800

    测试推送

--- atguigu-redis-jedis/src/main/java/com/atguigu/git/TestGit.java
+++ atguigu-redis-jedis/src/main/java/com/atguigu/git/TestGit.java
@@ -7,7 +7,7 @@ public class TestGit {
         System.out.println("Hello~");// edit by hotfix
         System.out.println("Hello~");// edit by master
         System.out.println("Hello~");// edit by resolve confict
-        System.out.println("Hello~");
+        System.out.println("Hello~");// test push
         System.out.println("Hello~");
         System.out.println("Hello~");
         System.out.println("Hello~");
