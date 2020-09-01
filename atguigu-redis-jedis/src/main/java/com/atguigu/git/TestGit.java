c543bbb16454ad80fe744425b010c698be166bd1
justin <justin@atguigu.com>
2020-09-01 16:20:34 +0800

    创建了一个类：TestGit

---
+++ atguigu-redis-jedis/src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello~");
+        System.out.println("Hello~");
+        System.out.println("Hello~");
+        System.out.println("Hello~");
+        System.out.println("Hello~");
+        System.out.println("Hello~");
+        System.out.println("Hello~");
+    }
+
+}
